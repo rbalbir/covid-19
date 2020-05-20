f41889f8947a175c322f25802437d26105918969
rodrigo <rbalbi2012@gmail.com>
2020-05-20 11:33:45 -0300

    Build

---
+++ build/precache-manifest.aa9a7ecc4e50a60002a823ad87e73c4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8015b2903f65b41b63f8f02ac74529b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03ef5c5ac610a66063f3",
+    "url": "/static/css/main.47815891.chunk.css"
+  },
+  {
+    "revision": "b8e412137531f1481498",
+    "url": "/static/js/2.216769d7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.216769d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ef5c5ac610a66063f3",
+    "url": "/static/js/main.fd9eae84.chunk.js"
+  },
+  {
+    "revision": "b5da41a4f09a10b25f0b",
+    "url": "/static/js/runtime-main.7ca5be63.js"
+  }
+]);
